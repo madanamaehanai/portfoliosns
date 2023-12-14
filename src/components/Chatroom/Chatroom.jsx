c22790f6cc87d049a5132fdf231858237062858d
madanamaehanai <homehack@outlook.jp>
2023-12-14 16:25:31 +0900

    chat修正

--- src/components/Chatroom/Chatroom.jsx
+++ src/components/Chatroom/Chatroom.jsx
@@ -8,13 +8,37 @@ function Chatroom() {
       <Link to="/" className="topbarLink" style={{ textDecoration: "none" }}>
         ホーム
       </Link>
-      <Link
-        to="/jobhunting"
-        className="topbarLink"
-        style={{ textDecoration: "none" }}
-      >
-        就活
-      </Link>
+      <div>
+        <Link
+          to="/jobhunting"
+          className="topbarLink"
+          style={{ textDecoration: "none" }}
+        >
+          就活
+        </Link>
+        <div>
+          <div>
+            <p>会社情報</p>
+            <div>
+              <p>自社開発</p>
+              <p>受託開発</p>
+              <p>SES</p>
+            </div>
+          </div>
+          <div>
+            <p>面接対策</p>
+            <div>
+
+            </div>
+          </div>
+          <div>
+            <p>ポートフォリオ</p>
+            <div>
+
+            </div>
+          </div>
+        </div>
+      </div>
       <Link
         to="/learning"
         className="topbarLink"
