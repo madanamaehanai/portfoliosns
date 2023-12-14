f0066cb6315e94b321bbe18fca2b6a2ae51ee645
madanamaehanai <homehack@outlook.jp>
2023-12-14 15:40:08 +0900

    フォロー実装

--- src/components/AuthorPost/Authorpost.jsx
+++ src/components/AuthorPost/Authorpost.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import axiosInstance from "../../axios";
+import { useContext } from "react";
+import { AuthContext } from "../../state/AuthContext";
 
 function Authorpost({ post }) {
-    console.log(post);
+    const { user } = useContext(AuthContext);
+    
+    // console.log(post);
+
+    const handleFollowToggle = async () => {
+
+        try {
+            await axiosInstance.post(`/user/forrow/${user._id}/${post._id}`);
+
+          } catch (err) {
+            console.log(err);
+          }
+      };
   return (
     <>
       <div className="postText">ユーザー: {post.username}</div>
+      <button onClick={handleFollowToggle}>フォロー：アンフォロー</button>
     </>
   );
 }
