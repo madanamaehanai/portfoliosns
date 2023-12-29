5a8187c7b3d8e3017d66b627576ffd4e7921f9e4
madanamaehanai <homehack@outlook.jp>
2023-12-29 21:48:55 +0900

    面接対策ページ作成

--- src/components/Preparation/Preparation.jsx
+++ src/components/Preparation/Preparation.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
 
 function Preparation() {
+  //投稿
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // const newPost = {
+    //   desc: e.target.elements.companyPostdesc.value,
+    //   userId: user._id,
+    //   companyid: `${companyId}`,
+    //   tag: category,
+    // };
+
+    // try {
+    //   await axiosInstance.post("/companypost", newPost);
+    //   window.location.reload();
+    //   console.log("投稿成功");
+    // } catch (err) {
+    //   console.log(err);
+    // }
+  };
   return (
-    <div>Preparation</div>
-  )
+    <div>
+      <div>Preparation</div>
+      <details>
+        <summary>エンジニアを目指した理由は？</summary>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input type="text" name="companyPostdesc" placeholder="企業名" />
+          <button>投稿</button>
+        </form>
+        {/* <div className="PostsComponents">
+            {incomes.map((post) => (
+              <Post post={post} key={post._id} />
+            ))}
+          </div> */}
+      </details>
+    </div>
+  );
 }
 
-export default Preparation
+export default Preparation;
