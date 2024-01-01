d179ae5fa05ddc5b9ed68117120030457b685f35
madanamaehanai <homehack@outlook.jp>
2024-01-02 06:48:43 +0900

    プログラミングページ追記

--- src/components/LearnComponents/LearnComponents.jsx
+++ src/components/LearnComponents/LearnComponents.jsx
@@ -20,8 +20,20 @@ function LearnComponents() {
           <details>
             <summary>React</summary>
             <p>
-              採用企業例（Facebook, Instagram, Yahoo!, Airbnb, Reddit, Netflix,
-              Slack, Uber）
+              採用事例（Facebook, Instagram, Yahoo!, Airbnb, Reddit, Netflix,
+              Slack, Uber, Skype）
+            </p>
+            <p>
+              Reactのメリット1
+              宣言的でソースの可読性が高い、Reactはページのパーツごとにコードを分けられるので、第三者がソースコードを読んでもコードを理解しやすく、不具合の発券や修復が容易となる。
+            </p>
+            <p>
+              Reactのメリット2
+              コンポーネント管理の為、拡張性が容易であり、コンポーネント指向でないプログラムはコードの全体に変更時の影響が発生しうるが、コンポーネント管理の場合は局所的な変更対応で済む。
+            </p>
+            <p>
+              Reactのメリット3 仮想ＤＯＭ採用の為、処理が高速
+              仮想DOMの為、Web描画時等に変更箇所のみ反映されるので、ページ全体を再描画するよりも局所的な処理で済むため、処理が高速
             </p>
           </details>
           <details>
