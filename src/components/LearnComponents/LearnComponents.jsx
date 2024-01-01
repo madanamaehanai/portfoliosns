54d74e337d27d84a6b90b961bb448ec3a6e1dca5
madanamaehanai <homehack@outlook.jp>
2024-01-01 21:31:42 +0900

    追記

--- src/components/LearnComponents/LearnComponents.jsx
+++ src/components/LearnComponents/LearnComponents.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
 function LearnComponents() {
   return (
-    <div className="CenterComponents">LearnComponents</div>
-  )
+    <div className="CenterComponents">
+      <div>LearnComponents</div>
+      <details>
+        <summary>マークアップ言語</summary>
+        <details>
+          <summary>HTML</summary>
+        </details>
+        <details>
+          <summary>CSS</summary>
+        </details>
+      </details>
+      <details>
+        <summary>JavaScript</summary>
+        <details>
+          <summary>フレームワーク</summary>
+          <details>
+            <summary>React</summary>
+            <p>
+              採用企業例（Facebook, Instagram, Yahoo!, Airbnb, Reddit, Netflix,
+              Slack, Uber）
+            </p>
+          </details>
+          <details>
+            <summary>React Native</summary>
+          </details>
+          <details>
+            <summary>Vue.js</summary>
+          </details>
+        </details>
+      </details>
+    </div>
+  );
 }
 
-export default LearnComponents
+export default LearnComponents;
