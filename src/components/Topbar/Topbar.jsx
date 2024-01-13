4189837042845d276114ae95a1649f9e3161ce63
madanamaehanai <homehack@outlook.jp>
2024-01-13 18:39:50 +0900

    画面移動している？

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -7,19 +7,59 @@ function Topbar() {
   const { user, dispatch } = useContext(AuthContext);
 
   const handleSubmit = async (e) => {
-    e.preventDefault(); //リロード回避
+    e.preventDefault(); // リロード回避
 
     logoutCall(dispatch);
   };
 
+  const handleSubmitmobile = async (e) => {
+    e.preventDefault(); // リロード回避
+    const mobileMenu = new MobileMenu(); // ここで MobileMenu インスタンスを作成
+    // mobileMenu.init();
+  };
+
+  // MobileMenu クラスの定義も Topbar コンポーネントの中に移動
+
+  class MobileMenu {
+    constructor() {
+      this.DOM = {};
+      this.DOM.btn = document.querySelector(".mobile-menu__btn");
+      this.DOM.cover = document.querySelector(".mobile-menu__cover");
+      this.DOM.container = document.querySelector("#global-container");
+      this.eventType = this._getEventType();
+      this._addEvent();
+      // console.log(this.DOM);
+    }
+
+    _getEventType() {
+      const isTouchCapable = "ontouchstart" in window;
+      return isTouchCapable ? "touchstart" : "click";
+    }
+
+    _toggle() {
+      this.DOM.container.classList.toggle("menu-open");
+    }
+
+    _addEvent() {
+      this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
+      this.DOM.cover.addEventListener(this.eventType, this._toggle.bind(this));
+    }
+  }
 
   return (
     <div className="TopbarComponents">
+      {/* <div id="global-container">
+        <div id="container"></div>
+        <nav className="mobile-menu__cover"></nav>
+      </div> */}
       <form onSubmit={(e) => handleSubmit(e)}>
         <div className="Topbar"></div>
         <div>Topbar</div>
         <div>{user.username}</div>
         <button>ログアウト</button>
+      </form>
+      <form onSubmit={(e) => handleSubmitmobile(e)}>
+        <button className="mobile-menu__btn">モバイルボタン</button>
       </form>
     </div>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,14 +7,43 @@ import CenterComponents from "../../components/CenterComponents/CenterComponents
 
 function Home() {
   return (
-    <>
-      <Topbar />
-      <div className="HomeComponents">
-        <LeftComponents />
-        <CenterComponents />
-        <RightComponents />
+    <div id="global-container">
+      <div id="container">
+        <Topbar />
+        <div className="HomeComponents">
+          <LeftComponents />
+          <CenterComponents />
+          <RightComponents />
+        </div>
       </div>
-    </>
+      <nav className="mobile-menu__cover">
+        <div className="logo">
+          {/* <img class="logo__img" src="images/logo.svg" /> */}
+          <span className="logo__stay">Stay</span>
+          <span className="logo__world">World</span>
+        </div>
+        <ul className="mobile-menu__main">
+          <li className="mobile-menu__item">
+            <a className="mobile-menu__link" href="#">
+              <span className="main-title">Our Service</span>
+              <span className="sub-title">サービスについて</span>
+            </a>
+          </li>
+          <li className="mobile-menu__item">
+            <a className="mobile-menu__link" href="#">
+              <span className="main-title">About Us</span>
+              <span className="sub-title">私たちについて</span>
+            </a>
+          </li>
+          <li className="mobile-menu__item">
+            <a className="mobile-menu__link" href="#">
+              <span className="main-title">Contact Us</span>
+              <span className="sub-title">コンタクト方法</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
