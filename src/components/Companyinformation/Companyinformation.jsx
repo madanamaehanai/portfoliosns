bfa40232db6b281b60720b2bb83cf6f234daa7fd
madanamaehanai <homehack@outlook.jp>
2024-01-14 14:23:15 +0900

    スタイル修正

--- src/components/Companyinformation/Companyinformation.jsx
+++ src/components/Companyinformation/Companyinformation.jsx
@@ -12,8 +12,8 @@ function Companyinformation() {
       <div className="companygroup">
       <div className="companygroup__range">
         <div className="companygroup__post">
-          <p>自社開発企業</p>
-          <p>自社でサービスの企画、開発を行うビジネス企業</p>
+          <p className="maintext">自社開発企業</p>
+          <p className="subtext">自社でサービスの企画、開発を行うビジネス企業</p>
           <Link
             to="/inhouse"
             className="topbarLink"
@@ -23,8 +23,8 @@ function Companyinformation() {
           </Link>
         </div>
         <div className="companygroup__post">
-          <p>受託開発企業</p>
-          <p>企業からのサービス開発依頼に対して、開発を行う企業</p>
+          <p className="maintext">受託開発企業</p>
+          <p className="subtext">企業からのサービス開発依頼に対して、開発を行う企業</p>
           <Link
             to="/outhouse"
             className="topbarLink"
@@ -34,8 +34,8 @@ function Companyinformation() {
           </Link>
         </div>
         <div className="companygroup__post">
-          <p>SES企業</p>
-          <p>
+          <p className="maintext">SES企業</p>
+          <p className="subtext">
             各企業の要望に対して、エンジニアを派遣して開発サポートを行う企業
           </p>
           <Link
