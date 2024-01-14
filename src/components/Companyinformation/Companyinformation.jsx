1a60d4e577524bc49178812b5eeaa04a0ef0a291
madanamaehanai <homehack@outlook.jp>
2024-01-14 11:19:10 +0900

    スタイル修正

--- src/components/Companyinformation/Companyinformation.jsx
+++ src/components/Companyinformation/Companyinformation.jsx
@@ -4,54 +4,51 @@ import { Link } from "react-router-dom";
 
 function Companyinformation() {
   return (
-    <>
-      <div>
+    <div className="companypage">
+      <div className="companypost">
         <div>Companyinformation</div>
         <AddCompany />
       </div>
-      <div>
-        <p>自社開発企業</p>
-        <p>自社でサービスの企画、開発を行うビジネス企業</p>
-        <Link
-          to="/inhouse"
-          className="topbarLink"
-          style={{ textDecoration: "none" }}
-        >
-          自社開発企業ルームへ
-        </Link>
+      <div className="companygroup">
+      <div className="companygroup__range">
+        <div className="companygroup__post">
+          <p>自社開発企業</p>
+          <p>自社でサービスの企画、開発を行うビジネス企業</p>
+          <Link
+            to="/inhouse"
+            className="topbarLink"
+            style={{ textDecoration: "none" }}
+          >
+            自社開発企業ルームへ
+          </Link>
+        </div>
+        <div className="companygroup__post">
+          <p>受託開発企業</p>
+          <p>企業からのサービス開発依頼に対して、開発を行う企業</p>
+          <Link
+            to="/outhouse"
+            className="topbarLink"
+            style={{ textDecoration: "none" }}
+          >
+            受託開発企業ルームへ
+          </Link>
+        </div>
+        <div className="companygroup__post">
+          <p>SES企業</p>
+          <p>
+            各企業の要望に対して、エンジニアを派遣して開発サポートを行う企業
+          </p>
+          <Link
+            to="/ses"
+            className="topbarLink"
+            style={{ textDecoration: "none" }}
+          >
+            SES企業ルームへ
+          </Link>
+        </div>
       </div>
-      <br></br>
-      <br></br>
-      <br></br>
-      <div>
-        <p>受託開発企業</p>
-        <p>企業からのサービス開発依頼に対して、開発を行う企業</p>
-        <Link
-          to="/outhouse"
-          className="topbarLink"
-          style={{ textDecoration: "none" }}
-        >
-          受託開発企業ルームへ
-        </Link>
       </div>
-      <br></br>
-      <br></br>
-      <br></br>
-      <div>
-        <p>SES企業</p>
-        <p>各企業の要望に対して、エンジニアを派遣して開発サポートを行う企業</p>
-        <Link
-          to="/ses"
-          className="topbarLink"
-          style={{ textDecoration: "none" }}
-        >
-          SES企業ルームへ
-        </Link>
-      </div>
-      <br></br>
-      <br></br>
-      <br></br>
-    </>
+    </div>
   );
 }
 
