569cd81c5dbcd305ab874e20da022b5d440f79b0
madanamaehanai <homehack@outlook.jp>
2024-01-16 20:48:30 +0900

    learn delete 実装

--- src/components/PostDelete/PostDelete.jsx
+++ src/components/PostDelete/PostDelete.jsx
@@ -1,32 +1,32 @@
-import React from 'react'
+import React from "react";
 import axiosInstance from "../../axios";
 
 function PostDelete({ post, pass }) {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        
-        try {
-          if(pass=="home"){
-            // console.log("homeから",pass);
-            await axiosInstance.post(`/post/delete/${post._id}`);
-          }else if(pass=="company"){
-            // console.log("companyから",pass);
-            await axiosInstance.post(`/companypost/delete/${post._id}`);
-          }else if(pass=="learn"){
-            // console.log("learnから",pass);
-          }
-          window.location.reload();
-          console.log("削除成功");
-        } catch (err) {
-          console.log(err);
-        }
-      };
+    try {
+      if (pass == "home") {
+        // console.log("homeから",pass);
+        await axiosInstance.post(`/post/delete/${post._id}`);
+      } else if (pass == "company") {
+        // console.log("companyから",pass);
+        await axiosInstance.post(`/companypost/delete/${post._id}`);
+      } else if (pass == "learn") {
+        // console.log("learnから",pass);
+        await axiosInstance.post(`/learn/delete/${post._id}`);
+      }
+      window.location.reload();
+      console.log("削除成功");
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <form onSubmit={(e) => handleSubmit(e)}>
-        <button type="submit">削除</button>
-      </form>
-  )
+      <button type="submit">削除</button>
+    </form>
+  );
 }
 
-export default PostDelete
+export default PostDelete;
